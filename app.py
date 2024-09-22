abbbcdda7c766f501bc5abc126fa704ff17458f9
Wucop228 <zhupa2006@mail.ru>
2024-09-22 22:34:45 +0300

    add Database

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f'<Item {self.name}>'
+
+@app.route('/')
+def index():
+    items = Item.query.all()
+    return render_template('index.html', items=items)
+
+@app.route('/add', methods=['POST'])
+def add_item():
+    name = request.form.get('name')
+    description = request.form.get('description')
+
+    if name and description:
+        new_item = Item(name=name, description=description)
+        db.session.add(new_item)
+        db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete_item(id):
+    item = Item.query.get(id)
+    if item:
+        db.session.delete(item)
+        db.session.commit()
+
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
